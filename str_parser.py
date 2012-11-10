b74391d3a164ca0a23e7c24d1457950259731121
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-11-11 00:10:39 +0100

    refactored time str parser added data size parser

--- str_parser.py
+++ str_parser.py
@@ -1,18 +1,41 @@
+##############################################
+# time
+##############################################
 def get_to_seconds_factor(time_unit):
+	units = {
+		's': 1,
+		'm': 60,
+		'h': 60 * 60,
+		'd': 60 * 60 * 24,
+	}
 	try:
-		return {
-			's': 1,
-			'm': 60,
-			'h': 60 * 60,
-			'd': 60 * 60 * 24,
-		}[time_unit]
+		return units[time_unit]
 	except (KeyError):
-		return ValueError("%s is not a valid input: <int>[s|m|h|d]")
+		return ValueError("%s is not a valid input: <int>[%s]" % (time_unit, "|".join(units.keys())))
 
 def extract_secounds(time_str):
 	to_sec_fac = get_to_seconds_factor(time_str[-1])
 	n = int(time_str[:len(time_str)-1])
 	return n * to_sec_fac
 
+##############################################
+# data sizes
+##############################################
 def get_to_kb_factor(data_size_unit):
-	pass
+	units = {
+		'K': 1,
+		'M': 1024,
+		'G': 1024 * 1024,
+		'T': 1024 * 1024 * 1024,
+	}
+	try:
+		return units[data_size_unit.upper()]
+	except (KeyError):
+		return ValueError("%s is not a valid input: <int>[%s]" % (data_size_unit, "|".join(units.keys())))
+
+def extract_kb(data_size_str):
+	to_kb_fac = get_to_kb_factor(data_size_str[-1])
+	n = int(data_size_str[:len(data_size_str)-1])
+	return n * to_kb_fac
+
+
--- tests/test_str_parser.py
+++ tests/test_str_parser.py
@@ -1,20 +1,21 @@
 from str_parser import *
 from nose.tools import raises
+from nose.tools import eq_
 
 def test_get_to_seconds_factor():
-	assert(get_to_seconds_factor('s') == 1)
-	assert(get_to_seconds_factor('m') == 60)
-	assert(get_to_seconds_factor('h') == 60 * 60)
-	assert(get_to_seconds_factor('d') == 60 * 60 * 24)
+	eq_(get_to_seconds_factor('s'), 1)
+	eq_(get_to_seconds_factor('m'), 60)
+	eq_(get_to_seconds_factor('h'), 60 * 60)
+	eq_(get_to_seconds_factor('d'), 60 * 60 * 24)
 
 def test_extract_secounds_valid_input():
-	assert(extract_secounds('1s') == 1)
-	assert(extract_secounds('15s') == 15)
-	assert(extract_secounds('1m') == 60)
-	assert(extract_secounds('10m') == 600)
-	assert(extract_secounds('1h') == 60 * 60)
-	assert(extract_secounds('1d') == 60 * 60 * 24)
-	assert(extract_secounds('1d') == 60 * 60 * 24)
+	eq_(extract_secounds('1s'), 1)
+	eq_(extract_secounds('15s'), 15)
+	eq_(extract_secounds('1m'), 60)
+	eq_(extract_secounds('10m'), 600)
+	eq_(extract_secounds('1h'), 60 * 60)
+	eq_(extract_secounds('1d'), 60 * 60 * 24)
+	eq_(extract_secounds('1d'), 60 * 60 * 24)
 
 @raises(ValueError)
 def test_extract_secounds_invalid_input():
@@ -23,3 +24,36 @@ def test_extract_secounds_invalid_input():
 	extract_secounds('1sa')
 	extract_secounds('s1')
 	extract_secounds('1x')
+
+
+def test_get_to_kb_factor_valid_input():
+	# KB
+	eq_(get_to_kb_factor('k'), 1)
+	eq_(get_to_kb_factor('K'), 1)
+
+	# MB
+	eq_(get_to_kb_factor('m'), 1024)
+	eq_(get_to_kb_factor('M'), 1024)
+
+	# GB
+	eq_(get_to_kb_factor('g'), 1024 * 1024)
+	eq_(get_to_kb_factor('G'), 1024 * 1024)
+
+	# TB
+	eq_(get_to_kb_factor('t'), 1024 * 1024 * 1024)
+	eq_(get_to_kb_factor('T'), 1024 * 1024 * 1024)
+
+def test_extract_kb():
+	eq_(extract_kb('1k'), 1)
+	eq_(extract_kb('12k'), 12)
+	eq_(extract_kb('1K'), 1)
+
+	eq_(extract_kb('1m'), 1024)
+	eq_(extract_kb('1M'), 1024)
+
+	eq_(extract_kb('1g'), 1024 * 1024)
+	eq_(extract_kb('1G'), 1024 * 1024)
+
+	eq_(extract_kb('1t'), 1024 * 1024 * 1024)
+	eq_(extract_kb('1T'), 1024 * 1024 * 1024)
+
